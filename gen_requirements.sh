903932aa2a781bd501cdd5b6c576ff839617718a
mark.qj <mark.qj@gmail.com>
2021-11-12 16:47:15 +0800

    Bug fix

--- gen_requirements.sh
+++ gen_requirements.sh
@@ -1,6 +1,8 @@
-pip install pipreqs -y
-pip install pip-tools -y
+pip install pipreqs
+pip install pip-tools
 # Generate requirements.in based on imports in python file
 pipreqs --ignore setup --mode no-pin --savepath requirements.in
+# requirements.manual contains libraries not detected by pipreqs, merge it with requirements.in
+cat requirements.manual >> requirements.in
 # Generate requirements.txt using requirements.in
 pip-compile requirements.in
